5d6126200bc8285e245a8ed7d5c79a83b4ab1e6b
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2023-11-29 23:19:31 +0900

    귀신 모델링 생성완료

---
+++ CG_PROJECT_-11a73dd7eda6bf24750c12593b6c6cf7b6b3e40e (1)/CG_PROJECT_-11a73dd7eda6bf24750c12593b6c6cf7b6b3e40e/CG_PROJECT_Backup-main/CG_SHADER/src/Flash.cpp
@@ -0,0 +1,52 @@
+#include "pch.h"
+#include "Flash.h"
+#include "Player.h"
+
+Flash::Flash(Model& model):Object(ObjectType::FLASH)
+{
+	_center = model.GetCenter();
+	_size = model.GetSize();
+	_first_center = _center;
+	_model = &model;
+}
+
+Flash::~Flash()
+{
+}
+
+void Flash::Init()
+{
+	Super::Init();
+}
+
+void Flash::Update()
+{
+
+	Super::Update();
+
+
+}
+
+void Flash::MatrixUpdate(Player* player)
+{
+	glm::vec3 cameraFront = GET_SINGLE(CameraManager)->m_cameraFront;
+	glm::vec4 flachloc = { player->GetCenter_x() + cameraFront.x , player->GetCenter_y() + cameraFront.y + 4.5f, player->GetCenter_z() + cameraFront.z, 1.0f };
+	glm::mat4 transMat = matrix::GetInstance()->GetTranslation(flachloc.x, flachloc.y, flachloc.z);
+
+	glm::mat4 yawMat = matrix::GetInstance()->GetRotate(GET_SINGLE(CameraManager)->m_cameraYaw, 0, 1, 0);
+	glm::mat4 pitchMat = matrix::GetInstance()->GetRotate(GET_SINGLE(CameraManager)->m_cameraPitch, 1, 0, 0);
+
+	_matrix = transMat * yawMat * pitchMat;
+}
+
+
+void Flash::Render(Shader& shader)
+{
+	if (lighton)
+	{
+
+		shader.SetUniform3f("control_color", 0, 0, 0);
+		shader.SetUniformMat4f("u_model", _matrix);
+		_model->RenderModel(shader);
+	}
+}
---
+++ CG_PROJECT_-11a73dd7eda6bf24750c12593b6c6cf7b6b3e40e (1)/CG_PROJECT_-11a73dd7eda6bf24750c12593b6c6cf7b6b3e40e/CG_PROJECT_Backup-main/CG_SHADER/src/Player.cpp
@@ -0,0 +1,176 @@
+#include "pch.h"
+#include "Player.h"
+#include "Object.h"
+#include "Object.h"
+#include "Collider.h"
+#include "Model.h"
+
+Player::Player(Model& model):Object(ObjectType::PLAYER)
+{
+	_center = model.GetCenter();
+	_size = model.GetSize();
+	_first_center = _center;
+	_model =&model;
+
+}
+
+Player::~Player()
+{
+
+}
+
+void Player::Init()
+{
+	Super::Init();
+
+
+}
+
+void Player::Update()
+{
+	KeyUpdate();
+
+	Super::Update();
+
+	MatrixUpdate();
+
+
+}
+
+void Player::Render(Shader& shader)
+{
+
+	Super::Render(shader);
+
+	shader.SetUniform3f("control_color", 0, 0, 0);
+	shader.SetUniformMat4f("u_model", _matrix);
+	_model->RenderModel(shader);
+
+}
+
+void Player::MatrixUpdate()
+{
+	_matrix = matrix::GetInstance()->GetTranslation(CameraManager::GetInstance()->m_cameraPos.x, CameraManager::GetInstance()->m_cameraPos.y, CameraManager::GetInstance()->m_cameraPos.z);
+
+
+}
+
+void Player::KeyUpdate()
+{
+	if(KeyManager::GetInstance()->Getbutton(KeyType::W))
+	{
+		_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+		_center.y = 0.0f;
+		_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+	}
+
+	if (KeyManager::GetInstance()->Getbutton(KeyType::S))
+	{
+		_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+		_center.y = 0.0f;
+		_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+	}
+
+	if (KeyManager::GetInstance()->Getbutton(KeyType::A))
+	{
+		_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+		_center.y = 0.0f;
+		_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+	}
+
+	if (KeyManager::GetInstance()->Getbutton(KeyType::D))
+	{
+		_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+		_center.y = 0.0f;
+		_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+	}
+}
+
+void Player::OnComponentBeginOverlap(Collider* collider, Collider* other)
+{
+
+	if (other->GetOwner()->GetObjectType() == ObjectType::WALL)
+	{
+		// 부딛힌 방향의 역방향 재이동 수행, 대각선은 아직 수정이 필요함
+		auto cameraRight = glm::normalize(glm::cross(CameraManager::GetInstance()->m_cameraUp, -(CameraManager::GetInstance()->m_cameraFront)));
+		float dt = TimeManager::GetInstance()->GetDeltaTime();
+
+		if (KeyManager::GetInstance()->Getbutton(KeyType::W))
+		{
+			// -- front
+
+			CameraManager::GetInstance()->m_cameraPos -= (CameraManager::GetInstance()->m_cameraSpeed) * CameraManager::GetInstance()->m_cameraFront * dt;
+			CameraManager::GetInstance()->m_cameraPos.y = 5.0f;
+
+			_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+			_center.y = 0.0f;
+			_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+		}
+		if (KeyManager::GetInstance()->Getbutton(KeyType::S))
+		{
+			// ++ front
+
+			CameraManager::GetInstance()->m_cameraPos += (CameraManager::GetInstance()->m_cameraSpeed) * CameraManager::GetInstance()->m_cameraFront * dt;
+			CameraManager::GetInstance()->m_cameraPos.y = 5.0f;
+
+			_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+			_center.y = 0.0f;
+			_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+		}
+
+		if (KeyManager::GetInstance()->Getbutton(KeyType::A))
+		{
+			// ++ right
+
+			CameraManager::GetInstance()->m_cameraPos += (CameraManager::GetInstance()->m_cameraSpeed) * cameraRight * dt;
+
+			_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+			_center.y = 0.0f;
+			_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+		}
+		if (KeyManager::GetInstance()->Getbutton(KeyType::D))
+		{
+			// -- right
+			CameraManager::GetInstance()->m_cameraPos -= (CameraManager::GetInstance()->m_cameraSpeed) * cameraRight * dt;
+
+			_center.x = CameraManager::GetInstance()->m_cameraPos.x;
+			_center.y = 0.0f;
+			_center.z = CameraManager::GetInstance()->m_cameraPos.z;
+
+		}
+
+		CameraManager::GetInstance()->m_cameraSpeed = 0.0f;
+
+		/////////////////////////////////////////////////////////////
+	}
+
+
+	_debug_color.x = 0;
+	_debug_color.y = 1;
+	_debug_color.z = 0;
+
+
+
+
+}
+
+void Player::OnComponentEndOverlap(Collider* collider, Collider* other)
+{
+	
+
+	_debug_color.x = 1;
+	_debug_color.y = 0;
+	_debug_color.z = 0;
+
+	// 속도 원상복귀
+	CameraManager::GetInstance()->m_cameraSpeed = 40.0f;
+
+
+
+}
