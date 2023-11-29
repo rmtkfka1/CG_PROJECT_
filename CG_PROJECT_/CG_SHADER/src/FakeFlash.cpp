3b63816e7b5eec41a3a182e90def2249af260860
rmtkfka1 <132369391+rmtkfka1@users.noreply.github.com>
2023-11-30 04:42:24 +0900

    모델링 수정 + 캐릭터 높이 10으로 수정

---
+++ CG_PROJECT_/CG_SHADER/src/FakeFlash.cpp
@@ -0,0 +1,79 @@
+#include "pch.h"
+#include "FakeFlash.h"
+#include "BoxCollider.h"
+#include "Light2.h"
+#include "Flash.h"
+
+FakeFlash::FakeFlash(Model& model):Object(ObjectType::FAKE_FLASH)
+{
+	_center = model.GetCenter();
+	_size.x = model.GetSize().x+10;
+	_size.z = model.GetSize().z+10;
+	_first_center = _center;
+	_model = &model;
+
+}
+
+FakeFlash::~FakeFlash()
+{
+}
+
+void FakeFlash::Init()
+{
+	Super::Init();
+}
+
+void FakeFlash::Update()
+{
+
+	Super::Update();
+
+
+}
+
+void FakeFlash::UpdateFlash(Light2* light, Flash* flash)
+{
+	if (!draw)
+	{
+		flash->SetLighton();
+
+		light->Spot_light.distance = 500.0f;
+
+		light->Spot_light.cutoff[0] = -10.0f;
+		light->Spot_light.cutoff[1] = 30.0f;
+
+		light->point_light.diffuse = glm::vec3(1.0f, 0, 0);
+		light->point_light.distance = 200.0f;
+	}
+	
+}
+
+void FakeFlash::Render(Shader& shader)
+{
+	
+	if (draw)
+	{
+		
+		Super::Render(shader); 
+
+		
+		shader.SetUniform3f("control_color", 0, 0, 0);
+		shader.SetUniformMat4f("u_model", _matrix);
+		_model->RenderModel(shader);
+	}
+}
+
+void FakeFlash::OnComponentBeginOverlap(Collider* collider, Collider* other)
+{
+
+	if (other->GetOwner()->GetObjectType() == ObjectType::PLAYER)
+	{
+		draw = false;
+	}
+}
+
+void FakeFlash::OnComponentEndOverlap(Collider* collider, Collider* other)
+{
+
+
+}
---
+++ CG_PROJECT_/CG_SHADER/src/Stage1.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "Scene.h"
+
+
+class Player;
+class Model;
+class Billboard;
+class Texture;
+class Shader;
+class Wall;
+class Light2;
+class Light_test;
+class Object;
+class Flash;
+class FakeFlash;
+class Ghost;
+
+class Stage1 : public Scene
+{
+public:
+	Stage1();
+	virtual ~Stage1();
+
+	virtual void Init() override;
+	virtual void Update() override;
+	virtual void Render() override;
+
+
+	
+	void Object_Render();
+	void Texture_Render();
+
+private:
+	void MakeRoom();
+	void MakeRoom3();
+	void MakeCorridor2();
+	void MakeRoom2();
+	void MakeCorridor();
+	void MakeRoom1();
+	void ObjectInit();
+
+private:
+
+	int screenWidth = glutGet(GLUT_SCREEN_WIDTH); // 모니터의 가로 해상도 가져오기
+	int screenHeight = glutGet(GLUT_SCREEN_HEIGHT); // 모니터의 세로 해상도 가져오기
+
+
+	//쉐이더
+	Shader* shader;
+	
+	
+	//각기 다른 텍스쳐를 적용해줘야 되는 애들//
+	Player* player;
+	Flash* flash;
+	FakeFlash* fake_flash;
+	Billboard* billboard;
+	Wall* table;
+	Ghost* ghost;
+	////////////////////
+
+	
+	// pair로 Object와 Model을 묶어버림
+	vector<Wall*> room1;
+
+	// pair로 Object와 Model을 묶어버림
+	vector<Wall*> corridor1;
+
+	// pair로 Object와 Model을 묶어버림
+	vector<Wall*> room2;
+
+	// pair로 Object와 Model을 묶어버림
+	vector<Wall*> corridor2;
+
+	// pair로 Object와 Model을 묶어버림
+	vector<Wall*> room3;
+
+
+	//텍스쳐
+	Texture* texture;
+	Texture* billboard_texture;
+	Texture* light_texture;
+	Texture* table_texture;
+	Texture* flash_fake_texture;
+	Texture* ghost_texture;
+
+	//조명 객체 만들기
+	Light2* light;
+
+
+
+
+};
+
